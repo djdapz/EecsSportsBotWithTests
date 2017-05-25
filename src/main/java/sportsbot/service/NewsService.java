ba6e8ccb94f9c678e3fdc2b643c8c70ecc36ff4b
wlj2100 <wlj2100@gmail.com>
2017-05-25 15:30:53 -0500

    News almost done
    Remove white list since not working
    Use bing news search instead

--- src/main/java/sportsbot/service/NewsService.java
+++ src/main/java/sportsbot/service/NewsService.java
@@ -19,12 +19,56 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 // goose package
 import com.gravity.goose.*;
+// rake
+
+// list
+import java.util.*;
+
 /**
  * Created by devondapuzzo on 5/15/17.
+ */
+/*
+TODO:
+add white list site:(espn.com)
+extract main content by goose
+extract key words
+
  */
 @Service
 public class NewsService
 {
+    private static List<String> whiteList = new ArrayList<String>();
+    public NewsService() {
+        whiteList.add("espn");
+        whiteList.add("yahoo");
+        whiteList.add("cbssports");
+//        whiteList.add("nbcsports");
+//        whiteList.add("foxsports");
+//        whiteList.add("cnn");
+//        whiteList.add("nytimes");
+//        whiteList.add("bleacherreport");
+//        whiteList.add("sbnation");
+//        whiteList.add("rantsports");
+    }
+    private static String getWhiteListString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("site:(");
+        for (Object str : whiteList.toArray()) {
+//            sb.append("");
+            sb.append(str);
+            sb.append(".com ");
+        }
+        sb.deleteCharAt(sb.length()-1);
+        sb.append(")");
+        return sb.toString();
+    }
+    private static boolean checkWhiteList(String url) {
+        for (String whiteName : whiteList) {
+//            System.out.println(whiteName);
+            if (url.contains(whiteName)) return true;
+        }
+        return false;
+    }
     public Story getNewsStory(QuestionContext qc){
         assert(qc.getQuestionType() == QuestionType.NEWS);
 //        assert(qc.getTeam() != null);
@@ -34,72 +78,84 @@ public class NewsService
         assert(!qc.isClarification());
 
         StringBuilder sb = new StringBuilder();
+//        sb.append(getWhiteListString());
+//        sb.append(" ");
         if (qc.getPlayer() != null) {
             sb.append(qc.getPlayer().getFirstName());
-            sb.append("+");
+            sb.append(" ");
             sb.append(qc.getPlayer().getLastName());
-            sb.append("+");
+            sb.append(" ");
             sb.append(qc.getTeam().getName());
-            sb.append("+");
-            sb.append(qc.getTemporalContext().toString());
+//            sb.append("+");
+//            sb.append(qc.getTemporalContext().toString());
         } else if (qc.getTeam() != null){
             sb.append(qc.getTeam().getName());
             if (qc.getTeam().getCity() != null) {
-                sb.append("+");
+                sb.append(" ");
                 sb.append(qc.getTeam().getCity().getName());
             }
-            sb.append("+").append(qc.getTemporalContext().toString());
+//            sb.append(" ").append(qc.getTemporalContext().toString());
         } else if (qc.getPreviousQuestion().getPlayer() != null) {
             sb.append(qc.getPreviousQuestion().getPlayer().getFirstName());
-            sb.append("+");
+            sb.append(" ");
             sb.append(qc.getPreviousQuestion().getPlayer().getLastName());
-            sb.append("+");
+            sb.append(" ");
             sb.append(qc.getPreviousQuestion().getTeam().getName());
-            sb.append("+");
-            sb.append(qc.getPreviousQuestion().getTemporalContext().toString());
+//            sb.append("+");
+//            sb.append(qc.getPreviousQuestion().getTemporalContext().toString());
         } else {
             sb.append(qc.getPreviousQuestion().getTeam().getName());
             if (qc.getPreviousQuestion().getTeam().getCity() != null) {
-                sb.append("+");
+                sb.append(" ");
                 sb.append(qc.getPreviousQuestion().getTeam().getCity().getName());
             }
-            sb.append("+").append(qc.getPreviousQuestion().getTemporalContext().toString());
+//            sb.append("+").append(qc.getPreviousQuestion().getTemporalContext().toString());
         }
-
+//        https://www.bing.com/search?q=site%3A+%28espn.com+yahoo.com+cbssports.com+nbcsports.com+foxsports.com+cnn.com+nytimes.com+bleacherreport.com+sbnation.com+rantsports.com+%29+Cubs%2BChicago%2BTODAY&go=Submit&qs=n&form=QBLH&sp=-1&pq=site%3A+%28espn.com+yahoo.com+cbssports.com+nbcsports.com+foxsports.com+cnn.com+nytimes.com+bleacherreport.com+sbnation.com+rantsports.com+%29+cubs%2Bchicago%2Btoday&sc=0-155&sk=&cvid=164E1EF275874899BEBD56A50F818D5C
+//        System.out.println(sb.toString());
         HttpClient httpclient = HttpClients.createDefault();
 
         try {
-            URIBuilder builder = new URIBuilder("https://api.cognitive.microsoft.com/bing/v5.0/search");
-
+//            URIBuilder builder = new URIBuilder("https://api.cognitive.microsoft.com/bing/v5.0/search");
+            URIBuilder builder = new URIBuilder(("https://api.cognitive.microsoft.com/bing/v7.0/news/search"));
             builder.setParameter("q", sb.toString());
-//            builder.setParameter("count", "1");
+            builder.setParameter("count", "20");
             builder.setParameter("mkt", "en-us");
-//            builder.setParameter("safesearch", "Moderate");
+            builder.setParameter("safesearch", "Moderate");
 
             URI uri = builder.build();
+            System.out.println(uri.toString());
             HttpGet request = new HttpGet(uri);
-            request.setHeader("Ocp-Apim-Subscription-Key", "9f4ef49129f345138d77a8bc5c599923");
+            request.setHeader("Ocp-Apim-Subscription-Key", "e9f923377a944bc8885330a050af76f1");
 
             HttpResponse response = httpclient.execute(request);
             HttpEntity entity = response.getEntity();
             if (entity != null) {
                 JSONObject temp = new JSONObject(EntityUtils.toString(entity));
-//                System.out.println(temp.toString(4));
-//                System.out.println(temp.keys().toString());
-
-                JSONObject temp2 = (JSONObject)temp.get(temp.keys().next());
-//                System.out.println(temp2.toString(4));
-                JSONArray news = temp2.getJSONArray("value");
-//                System.out.println(((JSONObject)news.get(0)).keySet());
-//                System.out.println(((JSONObject)news.get(0)).getString("name"));
-//                System.out.println(((JSONObject)news.get(0)).getString("description"));
-//                System.out.println(((JSONObject)news.get(0)).getString("url"));
-
-
+                System.out.println(temp.toString(4));
+//                System.out.println(temp.keySet().toString());
+//                JSONObject temp2 = null;
+//                if (temp.has("news")) {
+//                    temp2 = (JSONObject)temp.get("news");
+//                } else if (temp.has("webPages")) {
+//                    temp2 = (JSONObject)temp.get("webPages");
+//                } else {
+//                    return null;
+//                }
+//                JSONArray news = temp2.getJSONArray("value");
+//                System.out.print(news.toString(4));
+                JSONArray news = temp.getJSONArray("value");
+                String url = null;
                 Integer newsKey = (int)Math.floor(Math.random() * news.length());
-
-                String url = ((JSONObject)news.get(newsKey)).getString("url");
-
+                url = ((JSONObject)news.get(newsKey)).getString("url");
+//                do {
+//                    Integer newsKey = (int)Math.floor(Math.random() * news.length());
+//                    url = ((JSONObject)news.get(newsKey)).getString("url");
+//                    news.remove(newsKey);
+////                    System.out.println(url);
+//                } while(!checkWhiteList(url) && news.length() > 0);
+//
+//                if (news.length() == 0) return null;
 
                 Configuration configuration = new Configuration();
                 configuration.setMinBytesForImages(4500);
@@ -109,10 +165,15 @@ public class NewsService
                 Goose goose = new Goose(configuration);
                 Article article = goose.extractContent(url);
 //                System.out.println(article.cleanedArticleText());
-                return new Story(article.cleanedArticleText(), url);
+                if (article.cleanedArticleText().length() < 20) {
+                    return getNewsStory(qc);
+                } else {
+                    return new Story(article.cleanedArticleText().length() > 300? article.cleanedArticleText().substring(0, 300):article.cleanedArticleText(), url);
+                }
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
+//            getNewsStory(qc);
         }
         return null;
     }
